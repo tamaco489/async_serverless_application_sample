6403af390063c746d1edf3d67e6d402c2e78b583
Masato Takeuchi <tamaco489@gmail.com>
2025-03-02 05:19:55 +0900

    feature ecr (#2)
    * fix Dockerfile go version
    
    * fix api Makefile
    
    * add ecr resource
    
    * add .terraform.lock.hcl
    
    ---------
    
    Co-authored-by: takeuchima0 <miyabiii1210@gmail.com>

---
+++ infra/ecr/ecr.tf
@@ -0,0 +1,63 @@
+resource "aws_ecr_repository" "shop_api" {
+  name = "${local.fqn}-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name  = "${local.fqn}-api"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "shop_api" {
+  repository = aws_ecr_repository.shop_api.name
+  policy = jsonencode(
+    {
+      "rules" : [
+        {
+          "rulePriority" : 1,
+          "description" : "バージョン付きのイメージを5個保持する、6個目がアップロードされた際には古いものから順に削除されていく",
+          "selection" : {
+            "tagStatus" : "tagged",
+            "tagPrefixList" : ["shop_api_v"],
+            "countType" : "imageCountMoreThan",
+            "countNumber" : 5
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        },
+        {
+          "rulePriority" : 2,
+          "description" : "タグが設定されていないイメージをアップロードされてから3日後に削除する",
+          "selection" : {
+            "tagStatus" : "untagged",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 3
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        },
+        {
+          "rulePriority" : 3,
+          "description" : "タグが設定されたイメージをアップロードされてから7日後に削除する",
+          "selection" : {
+            "tagStatus" : "any",
+            "countType" : "sinceImagePushed",
+            "countUnit" : "days",
+            "countNumber" : 7
+          },
+          "action" : {
+            "type" : "expire"
+          }
+        }
+      ]
+    }
+  )
+}
