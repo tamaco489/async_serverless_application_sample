6c0d7dfaeb9f2f7547bbe5dff0f1d992dc8ec2ef
Masato Takeuchi <tamaco489@gmail.com>
2025-03-02 05:51:27 +0900

    feature network (#5)
    * add acm resource
    
    * fix comment
    
    * add network resource
    
    ---------
    
    Co-authored-by: tamaco489 <tamaco489@gmail.com>

---
+++ infra/network/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc" {
+  value = {
+    arn                = aws_vpc.async_serverless_app.arn
+    id                 = aws_vpc.async_serverless_app.id
+    cidr_block         = aws_vpc.async_serverless_app.cidr_block
+    # public_subnet_ids  = [for s in aws_subnet.public_subnet : s.id]
+    private_subnet_ids = [for s in aws_subnet.private_subnet : s.id]
+  }
+}
