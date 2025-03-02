a25af4763b2436ec29db3357c54d2fce43a13374
tamaco489 <tamaco489@gmail.com>
2025-03-02 22:35:07 +0900

    setup push notification batch

---
+++ batch/push_notification/internal/handler/handler.go
@@ -0,0 +1,30 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/tamaco489/async_serverless_application_sample/batch/push_notification/internal/usecase"
+)
+
+func PushNotificationHandler(job usecase.Job) func(ctx context.Context, sqsEvent events.SQSEvent) error {
+
+	return func(ctx context.Context, sqsEvent events.SQSEvent) error {
+
+		for _, record := range sqsEvent.Records {
+			var message interface{}
+			if err := json.Unmarshal([]byte(record.Body), &message); err != nil {
+				slog.ErrorContext(ctx, "failed to unmarshal message", slog.String("error", err.Error()))
+				return err
+			}
+
+			if err := job.ProcessMessage(ctx, message); err != nil {
+				slog.ErrorContext(ctx, "failed to process message", slog.String("error", err.Error()))
+				return err
+			}
+		}
+		return nil
+	}
+}
