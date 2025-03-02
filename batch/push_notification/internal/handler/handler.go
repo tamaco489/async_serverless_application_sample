60afa13d541afec913f411655d0a1a5df6b5c5c6
tamaco489 <tamaco489@gmail.com>
2025-03-03 02:33:16 +0900

    fix comment for handler

--- batch/push_notification/internal/handler/handler.go
+++ batch/push_notification/internal/handler/handler.go
@@ -14,6 +14,15 @@ type SQSEventJob func(ctx context.Context, sqsEvent events.SQSEvent) error
 func PushNotificationHandler(job usecase.Job) SQSEventJob {
 	return func(ctx context.Context, sqsEvent events.SQSEvent) error {
 
+		// return fmt.Errorf("意図的にエラーにしています。")
+
+		/*
+			- 意図的にエラーを起こすことで、SQS側でリトライ処理が走る。
+			- 可視性タイムアウトの時間 × リトライ回数を過ぎた後にDLQにキューイングされる。
+			- なお、DLQに引き渡された後も処理ができなかった場合は、最大7日間保持され、その後は削除される。
+			- DLQにキューイングされたものは可視性タイムアウト終了後、内容を確認しAWS CLI等で手動で再送する/破棄する等の対応が必要となる。
+		*/
+
 		for _, record := range sqsEvent.Records {
 			var message purchaseQueueMessage
 			if err := json.Unmarshal([]byte(record.Body), &message); err != nil {
