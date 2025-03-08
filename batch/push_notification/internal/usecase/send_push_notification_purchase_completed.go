281f2189e174d5316ab2d6ace008b66bfb89458f
tamaco489 <tamaco489@gmail.com>
2025-03-08 22:29:10 +0900

    fix send push notification purchase completed

--- batch/push_notification/internal/usecase/send_push_notification_purchase_completed.go
+++ batch/push_notification/internal/usecase/send_push_notification_purchase_completed.go
@@ -2,10 +2,43 @@ package usecase
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"log/slog"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"github.com/tamaco489/async_serverless_application_sample/batch/push_notification/internal/configuration"
+	"github.com/tamaco489/async_serverless_application_sample/batch/push_notification/internal/models"
 )
 
-func (j *Job) SendPushNotificationPurchaseCompleted(ctx context.Context) error {
+func (j *Job) SendPushNotificationPurchaseCompleted(ctx context.Context, message models.PurchaseQueueMessage) error {
 	slog.InfoContext(ctx, "send push notification purchase completed...")
+
+	// push通知送信したいけどアプリないのでLINE Message APIを利用する。
+
+	channelSecret := configuration.Get().LineMessageAPI.ChannelSecret
+	channelAccessToken := configuration.Get().LineMessageAPI.ChannelAccessToken
+	luid := configuration.Get().LineMessageAPI.UserID
+
+	// todo:
+	log.Println("[info] line_message_api", "channel_secret:", channelSecret, "channel_access_token", channelAccessToken, "user_id:", luid)
+
+	bot, err := linebot.New(channelSecret, channelAccessToken)
+	if err != nil {
+		return fmt.Errorf("failed to new linebot client: %w", err)
+	}
+	if _, err = bot.PushMessage(luid, linebot.NewTextMessage(j.buildPushMessage(message))).Do(); err != nil {
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+
 	return nil
+}
+
+func (j *Job) buildPushMessage(message models.PurchaseQueueMessage) string {
+	return fmt.Sprintf(
+		"注文処理が完了しました。\n注文ID: %s\nユーザID: %d\n注文ステータス: %s",
+		message.OrderID,
+		message.UserID,
+		message.Status.String(),
+	)
 }
