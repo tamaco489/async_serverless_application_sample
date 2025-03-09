d5fc6a2f517a399312398d02d007a231b4852c3b
tamaco489 <tamaco489@gmail.com>
2025-03-09 22:23:19 +0900

    fix operation id

--- api/gem/internal/controller/get_currency_balance.go
+++ api/gem/internal/controller/get_currency_balance.go
@@ -5,9 +5,9 @@ import (
 	"github.com/tamaco489/async_serverless_application_sample/api/gem/internal/gen"
 )
 
-func (c *Controllers) GetCurrencyBalance(ctx *gin.Context, request gen.GetCurrencyBalanceRequestObject) (gen.GetCurrencyBalanceResponseObject, error) {
+func (c *Controllers) GetGemBalance(ctx *gin.Context, request gen.GetGemBalanceRequestObject) (gen.GetGemBalanceResponseObject, error) {
 
-	return gen.GetCurrencyBalance200JSONResponse{
+	return gen.GetGemBalance200JSONResponse{
 		All:  1500,
 		Paid: 1000,
 		Free: 500,
