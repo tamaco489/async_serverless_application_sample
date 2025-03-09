28d3424ea439c235245945f7ee5f41dd1bd1ac0a
tamaco489 <tamaco489@gmail.com>
2025-03-10 02:57:13 +0900

    fix scripts

--- api/gem/scripts/dynamodb/schema/create_user_info.sh
+++ api/gem/scripts/dynamodb/schema/create_user_info.sh
@@ -2,7 +2,13 @@
 
 aws --endpoint-url http://localhost:8000 dynamodb create-table \
     --table-name user_info \
-    --attribute-definitions AttributeName=user_id,AttributeType=S AttributeName=last_login,AttributeType=S AttributeName=level,AttributeType=N AttributeName=play_time,AttributeType=N \
+    --attribute-definitions \
+        AttributeName=user_id,AttributeType=S \
+        AttributeName=paid_gem_balance,AttributeType=N \
+        AttributeName=free_gem_balance,AttributeType=N \
+        AttributeName=level,AttributeType=N \
+        AttributeName=play_time,AttributeType=N \
+        AttributeName=last_login,AttributeType=S \
     --key-schema AttributeName=user_id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --global-secondary-indexes '[
@@ -29,6 +35,24 @@ aws --endpoint-url http://localhost:8000 dynamodb create-table \
         "KeySchema": [
           {"AttributeName": "user_id", "KeyType": "HASH"},
           {"AttributeName": "play_time", "KeyType": "RANGE"}
+        ],
+        "Projection": {"ProjectionType": "ALL"},
+        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+      },
+      {
+        "IndexName": "PaidGemBalanceIndex",
+        "KeySchema": [
+          {"AttributeName": "user_id", "KeyType": "HASH"},
+          {"AttributeName": "paid_gem_balance", "KeyType": "RANGE"}
+        ],
+        "Projection": {"ProjectionType": "ALL"},
+        "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+      },
+      {
+        "IndexName": "FreeGemBalanceIndex",
+        "KeySchema": [
+          {"AttributeName": "user_id", "KeyType": "HASH"},
+          {"AttributeName": "free_gem_balance", "KeyType": "RANGE"}
         ],
         "Projection": {"ProjectionType": "ALL"},
         "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
