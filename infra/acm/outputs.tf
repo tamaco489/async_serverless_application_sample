777b47f759caf4bcdecbbbf07c678c51ead05954
tamaco489 <tamaco489@gmail.com>
2025-03-11 00:23:25 +0900

    fix gem api http api gateway a recode

--- infra/acm/outputs.tf
+++ infra/acm/outputs.tf
@@ -16,3 +16,14 @@ output "shop_apigatewayv2_domain_name" {
     certificate_arn = aws_apigatewayv2_domain_name.shop_api_v1.domain_name_configuration[0].certificate_arn
   }
 }
+
+output "gem_apigatewayv2_domain_name" {
+  description = "details of the api gateway v2 custom domain name configuration."
+  value = {
+    id              = aws_apigatewayv2_domain_name.gem_api_v2.id
+    domain_name     = aws_apigatewayv2_domain_name.gem_api_v2.domain_name
+    endpoint_type   = aws_apigatewayv2_domain_name.gem_api_v2.domain_name_configuration[0].endpoint_type
+    security_policy = aws_apigatewayv2_domain_name.gem_api_v2.domain_name_configuration[0].security_policy
+    certificate_arn = aws_apigatewayv2_domain_name.gem_api_v2.domain_name_configuration[0].certificate_arn
+  }
+}
