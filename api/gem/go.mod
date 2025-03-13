b673528d26377e99d7f81df8fd623329c4d19cf6
tamaco489 <tamaco489@gmail.com>
2025-03-13 23:37:11 +0900

    ad return uuid

--- api/gem/go.mod
+++ api/gem/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.41.1
 	github.com/gin-contrib/cors v1.7.3
 	github.com/gin-gonic/gin v1.10.0
+	github.com/google/uuid v1.5.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/oapi-codegen/runtime v1.1.1
 )
--- api/gem/internal/usecase/update_gem_purchase.go
+++ api/gem/internal/usecase/update_gem_purchase.go
@@ -8,6 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/tamaco489/async_serverless_application_sample/api/gem/internal/gen"
 )
 
@@ -18,27 +19,30 @@ func (u *gemUseCase) UpdateGemPurchase(ctx *gin.Context, request gen.UpdateGemPu
 	// 本来はcontextなどからuidを取得する
 	playerID := "27110642"
 
-	// uidを使用して、`player_profiles` から対象のデータを取得
+	// todo: uidを使用して、`player_profiles` から対象のデータを取得
 
-	// データが存在する場合は更新、存在しない場合は新たに作成
+	// todo: データが存在する場合は更新、存在しない場合は新たに作成
 
 	// 本来はマスタを参照し、gem_idに対応するアイテムの個数を取得
 	var quantityByID uint32 = 300
 	paidGemBalance := request.Body.Quantity * quantityByID
 
-	// 既にデータが存在する場合は、元々保有していた個数に加算した状態でテーブルを更新
+	// todo: 既にデータが存在する場合は、元々保有していた個数に加算した状態でテーブルを更新
 
 	now := time.Now().Format(time.RFC3339)
 
+	// プレイ時間も固定、35時間を分単位で表現
+	playTime := 35 * 60
+
 	item := map[string]types.AttributeValue{
-		"player_id":        &types.AttributeValueMemberS{Value: playerID},
-		"paid_gem_balance": &types.AttributeValueMemberN{Value: strconv.FormatUint(uint64(paidGemBalance), 10)},
-		"free_gem_balance": &types.AttributeValueMemberN{Value: "0"}, // 有償通貨なので0固定
-		"level":            &types.AttributeValueMemberN{Value: "1"},
-		"play_time":        &types.AttributeValueMemberN{Value: "12345"}, // 一旦ハードコード
-		"last_login":       &types.AttributeValueMemberS{Value: now},
-		"created_at":       &types.AttributeValueMemberS{Value: now},
-		"updated_at":       &types.AttributeValueMemberS{Value: now},
+		"player_id":         &types.AttributeValueMemberS{Value: playerID},
+		"paid_gem_balance":  &types.AttributeValueMemberN{Value: strconv.FormatUint(uint64(paidGemBalance), 10)},
+		"free_gem_balance":  &types.AttributeValueMemberN{Value: "0"}, // 有償通貨なので0固定
+		"level":             &types.AttributeValueMemberN{Value: "1"}, // ユーザのレベルは一定の条件に応じて変動、一旦固定
+		"play_time_minutes": &types.AttributeValueMemberN{Value: strconv.FormatUint(uint64(playTime), 10)},
+		"last_login":        &types.AttributeValueMemberS{Value: now},
+		"created_at":        &types.AttributeValueMemberS{Value: now},
+		"updated_at":        &types.AttributeValueMemberS{Value: now},
 	}
 
 	dc := u.dynamoDBClient
@@ -51,9 +55,10 @@ func (u *gemUseCase) UpdateGemPurchase(ctx *gin.Context, request gen.UpdateGemPu
 	}
 
 	// NOTE: `transaction_histories` テーブルにログを作成
+	transactionID := uuid.New().String()
 
 	return gen.UpdateGemPurchase201JSONResponse{
 		Balance:       100000,
-		TransactionId: "tx_purchase_001",
+		TransactionId: transactionID,
 	}, nil
 }
