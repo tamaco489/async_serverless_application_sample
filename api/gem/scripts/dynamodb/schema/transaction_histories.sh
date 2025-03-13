1b31b515852b91accb70ed16c124fb52fce4cbbe
tamaco489 <tamaco489@gmail.com>
2025-03-14 04:02:21 +0900

    fix transaction_histories table

--- api/gem/scripts/dynamodb/schema/transaction_histories.sh
+++ api/gem/scripts/dynamodb/schema/transaction_histories.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
-
 aws --endpoint-url http://localhost:8000 dynamodb create-table \
     --table-name transaction_histories \
     --attribute-definitions \
         AttributeName=transaction_id,AttributeType=S \
-        AttributeName=timestamp,AttributeType=N \
+        AttributeName=timestamp,AttributeType=S \
         AttributeName=player_id,AttributeType=S \
         AttributeName=transaction_type,AttributeType=S \
-        AttributeName=gem_id,AttributeType=S \
-        AttributeName=paid_gem_amount,AttributeType=N \
-        AttributeName=free_gem_amount,AttributeType=N \
-        AttributeName=description,AttributeType=S \
+        AttributeName=gem_id,AttributeType=N \
+        AttributeName=paid_gem_quantity,AttributeType=N \
+        AttributeName=free_gem_quantity,AttributeType=N \
     --key-schema AttributeName=transaction_id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
     --global-secondary-indexes '[
@@ -26,8 +23,8 @@ aws --endpoint-url http://localhost:8000 dynamodb create-table \
         {
             "IndexName": "TransactionTypeIndex",
             "KeySchema": [
-                {"AttributeName": "transaction_type", "KeyType": "HASH"},
-                {"AttributeName": "timestamp", "KeyType": "RANGE"}
+                {"AttributeName": "player_id", "KeyType": "HASH"},
+                {"AttributeName": "transaction_type", "KeyType": "RANGE"}
             ],
             "Projection": {"ProjectionType": "ALL"},
             "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
@@ -35,35 +32,26 @@ aws --endpoint-url http://localhost:8000 dynamodb create-table \
         {
             "IndexName": "GemTransactionsIndex",
             "KeySchema": [
-                {"AttributeName": "gem_id", "KeyType": "HASH"},
-                {"AttributeName": "timestamp", "KeyType": "RANGE"}
+                {"AttributeName": "player_id", "KeyType": "HASH"},
+                {"AttributeName": "gem_id", "KeyType": "RANGE"}
             ],
             "Projection": {"ProjectionType": "ALL"},
             "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
         },
         {
-            "IndexName": "PaidGemAmountIndex",
+            "IndexName": "PaidGemQuantityIndex",
             "KeySchema": [
-                {"AttributeName": "paid_gem_amount", "KeyType": "HASH"},
-                {"AttributeName": "timestamp", "KeyType": "RANGE"}
+                {"AttributeName": "player_id", "KeyType": "HASH"},
+                {"AttributeName": "paid_gem_quantity", "KeyType": "RANGE"}
             ],
             "Projection": {"ProjectionType": "ALL"},
             "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
         },
         {
-            "IndexName": "FreeGemAmountIndex",
+            "IndexName": "FreeGemQuantityIndex",
             "KeySchema": [
-                {"AttributeName": "free_gem_amount", "KeyType": "HASH"},
-                {"AttributeName": "timestamp", "KeyType": "RANGE"}
-            ],
-            "Projection": {"ProjectionType": "ALL"},
-            "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
-        },
-        {
-            "IndexName": "DescriptionIndex",
-            "KeySchema": [
-                {"AttributeName": "description", "KeyType": "HASH"},
-                {"AttributeName": "timestamp", "KeyType": "RANGE"}
+                {"AttributeName": "player_id", "KeyType": "HASH"},
+                {"AttributeName": "free_gem_quantity", "KeyType": "RANGE"}
             ],
             "Projection": {"ProjectionType": "ALL"},
             "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
