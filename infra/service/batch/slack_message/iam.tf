9c87ac65e0642b79409fd8f5285bbbddcb099ccf
tamaco489 <tamaco489@gmail.com>
2025-03-14 21:35:09 +0900

    fix dynamodb iam policy for fetch stream arn

--- infra/service/batch/slack_message/iam.tf
+++ infra/service/batch/slack_message/iam.tf
@@ -19,8 +19,38 @@ resource "aws_iam_role" "slack_message_batch" {
   }
 }
 
-# https://docs.aws.amazon.com/ja_jp/aws-managed-policy/latest/reference/AWSLambdaVPCAccessExecutionRole.html
+# DOC: https://docs.aws.amazon.com/ja_jp/aws-managed-policy/latest/reference/AWSLambdaVPCAccessExecutionRole.html
 resource "aws_iam_role_policy_attachment" "slack_message_batch_execution_role" {
   role       = aws_iam_role.slack_message_batch.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# =================================================================
+# dynamodb policy
+# =================================================================
+data "aws_iam_policy_document" "slack_message_batch_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeStream",
+      "dynamodb:GetRecords",
+      "dynamodb:GetShardIterator",
+      "dynamodb:ListStreams"
+    ]
+    resources = [
+      # NOTE: 注意点として、ここで指定するARNは対象のテーブルのARNではなく、DynamoDB StreamのARNを指定する必要がある。
+      data.terraform_remote_state.dynamodb.outputs.transaction_histories.stream_arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "slack_message_batch_policy" {
+  name        = "${local.fqn}-batch-dynamodb-policy"
+  description = "Allows Batch Lambda to read from the transaction_histories DynamoDB Stream"
+  policy      = data.aws_iam_policy_document.slack_message_batch_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "slack_message_batch_role" {
+  role       = aws_iam_role.slack_message_batch.name
+  policy_arn = aws_iam_policy.slack_message_batch_policy.arn
 }
