2c3b6a592e4cd596cff630e8c72f3a6e5de8721d
tamaco489 <tamaco489@gmail.com>
2025-03-15 01:17:33 +0900

    fix usecase and handler

--- batch/slack_message/internal/usecase/job.go
+++ batch/slack_message/internal/usecase/job.go
@@ -2,12 +2,14 @@ package usecase
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/tamaco489/async_serverless_application_sample/batch/slack_message/internal/configuration"
 )
 
 type Jobber interface {
-	SendSlackMessagePurchaseStreamEvent(ctx context.Context) error
+	SendSlackMessagePurchaseStreamEvent(ctx context.Context, event SendSlackMessagePurchaseStreamEvent) error
 }
 
 var _ Jobber = (*Job)(nil)
@@ -16,4 +18,22 @@ type Job struct{}
 
 func NewJob(cfg configuration.Config) (*Job, error) {
 	return &Job{}, nil
+}
+
+type SendSlackMessagePurchaseStreamEvent struct {
+	Keys     map[string]events.DynamoDBAttributeValue `json:"Keys"`
+	NewImage map[string]events.DynamoDBAttributeValue `json:"NewImage"`
+}
+
+func NewSendSlackMessagePurchaseStreamEvent(keys map[string]events.DynamoDBAttributeValue, newImage map[string]events.DynamoDBAttributeValue) (SendSlackMessagePurchaseStreamEvent, error) {
+	if keys == nil {
+		return SendSlackMessagePurchaseStreamEvent{}, fmt.Errorf("keys cannot be nil")
+	}
+	if newImage == nil {
+		return SendSlackMessagePurchaseStreamEvent{}, fmt.Errorf("newImage cannot be nil")
+	}
+	return SendSlackMessagePurchaseStreamEvent{
+		Keys:     keys,
+		NewImage: newImage,
+	}, nil
 }
--- batch/slack_message/internal/usecase/send_slack_message_purchase_stream_event.go
+++ batch/slack_message/internal/usecase/send_slack_message_purchase_stream_event.go
@@ -2,12 +2,73 @@ package usecase
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"log/slog"
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
-func (j *Job) SendSlackMessagePurchaseStreamEvent(ctx context.Context) error {
+func (j *Job) SendSlackMessagePurchaseStreamEvent(ctx context.Context, event SendSlackMessagePurchaseStreamEvent) error {
 
-	slog.InfoContext(ctx, "start slack message usecase process.")
+	slog.InfoContext(ctx, "start slack message usecase process.", slog.Any("event_detail", event))
+
+	// Keysからplayer_idとtimestampを取得
+	playerID, err := getValue(event.Keys, "player_id", convertToInt)
+	if err != nil {
+		return fmt.Errorf("failed to get player_id from Keys: %w", err)
+	}
+
+	timestamp, err := getValue(event.Keys, "timestamp", convertToString)
+	if err != nil {
+		return fmt.Errorf("failed to get timestamp from Keys: %w", err)
+	}
+
+	// NewImageから必要なフィールドを取得
+	transactionID, err := getValue(event.NewImage, "transaction_id", convertToString)
+	if err != nil {
+		return fmt.Errorf("failed to get transaction_id from NewImage: %w", err)
+	}
+
+	description, err := getValue(event.NewImage, "description", convertToString)
+	if err != nil {
+		return fmt.Errorf("failed to get description from NewImage: %w", err)
+	}
+
+	// todo: slackメッセージ用に成形する
+	log.Println("[info:1] player_id:", playerID)
+	log.Println("[info:2] timestamp:", timestamp)
+	log.Println("[info:3] transaction_id:", transactionID)
+	log.Println("[info:4] description:", description)
 
 	return nil
+}
+
+func getValue[T any](source map[string]events.DynamoDBAttributeValue, key string, convertFunc func(events.DynamoDBAttributeValue) (T, error)) (T, error) {
+	if value, ok := source[key]; ok {
+		return convertFunc(value)
+	}
+	var zero T
+	return zero, fmt.Errorf("%s not found in source", key)
+}
+
+// convertToIntはDynamoDBの数値型(N)を整数に変換するユーティリティ関数
+func convertToInt(attribute events.DynamoDBAttributeValue) (int, error) {
+	if attribute.Number() != "" {
+		var value int
+		_, err := fmt.Sscanf(attribute.Number(), "%d", &value)
+		if err != nil {
+			return 0, fmt.Errorf("failed to convert string to int: %v", err)
+		}
+		return value, nil
+	}
+	return 0, fmt.Errorf("invalid number value")
+}
+
+// convertToStringはDynamoDBの文字列型(S)を文字列に変換するユーティリティ関数
+func convertToString(attribute events.DynamoDBAttributeValue) (string, error) {
+	if attribute.String() != "" {
+		return attribute.String(), nil
+	}
+	return "", fmt.Errorf("invalid string value")
 }
