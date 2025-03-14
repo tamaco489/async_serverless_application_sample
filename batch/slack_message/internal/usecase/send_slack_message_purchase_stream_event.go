05577daaa058ef905504cc927d09a63094ad4b97
tamaco489 <tamaco489@gmail.com>
2025-03-15 02:46:00 +0900

    fix usecase for send slack message

--- batch/slack_message/internal/usecase/send_slack_message_purchase_stream_event.go
+++ batch/slack_message/internal/usecase/send_slack_message_purchase_stream_event.go
@@ -3,17 +3,14 @@ package usecase
 import (
 	"context"
 	"fmt"
-	"log"
-	"log/slog"
+	"time"
 
+	"github.com/tamaco489/async_serverless_application_sample/batch/slack_message/internal/library/slack"
 	"github.com/tamaco489/async_serverless_application_sample/batch/slack_message/internal/utils/dynamodb_util"
 )
 
 func (j *Job) SendSlackMessagePurchaseStreamEvent(ctx context.Context, event SendSlackMessagePurchaseStreamEvent) error {
 
-	slog.InfoContext(ctx, "start slack message usecase process.", slog.Any("event_detail", event))
-
-	// Keysからplayer_idとtimestampを取得
 	playerID, err := dynamodb_util.GetValue(event.Keys, "player_id", dynamodb_util.ConvertToInt)
 	if err != nil {
 		return fmt.Errorf("failed to get player_id from Keys: %w", err)
@@ -24,7 +21,6 @@ func (j *Job) SendSlackMessagePurchaseStreamEvent(ctx context.Context, event Sen
 		return fmt.Errorf("failed to get timestamp from Keys: %w", err)
 	}
 
-	// NewImageから必要なフィールドを取得
 	transactionID, err := dynamodb_util.GetValue(event.NewImage, "transaction_id", dynamodb_util.ConvertToString)
 	if err != nil {
 		return fmt.Errorf("failed to get transaction_id from NewImage: %w", err)
@@ -35,11 +31,49 @@ func (j *Job) SendSlackMessagePurchaseStreamEvent(ctx context.Context, event Sen
 		return fmt.Errorf("failed to get description from NewImage: %w", err)
 	}
 
-	// todo: slackメッセージ用に成形する
-	log.Println("[info:1] player_id:", playerID)
-	log.Println("[info:2] timestamp:", timestamp)
-	log.Println("[info:3] transaction_id:", transactionID)
-	log.Println("[info:4] description:", description)
+	parsedTime, err := time.Parse(time.RFC3339, timestamp)
+	if err != nil {
+		return fmt.Errorf("failed to parse timestamp: %w", err)
+	}
+	formattedTime := parsedTime.Format("2006-01-02 15:04:05")
+
+	sma := newSendSlackMessageAttachment(playerID, formattedTime, transactionID, description)
+	if err := j.slackClient.SendMessage(ctx, sma.title, sma.username, sma.formatSlackMessage()); err != nil {
+		return err
+	}
 
 	return nil
+}
+
+type sendSlackMessageAttachment struct {
+	username      string
+	title         string
+	playerID      int
+	formattedTime string
+	transactionID string
+	description   string
+}
+
+func newSendSlackMessageAttachment(playerID int, formattedTime, transactionID, description string) sendSlackMessageAttachment {
+	username, title := "購入が完了しました。", "tamaco489"
+	return sendSlackMessageAttachment{
+		username:      username,
+		title:         title,
+		playerID:      playerID,
+		formattedTime: formattedTime,
+		transactionID: transactionID,
+		description:   description,
+	}
+}
+
+func (sa sendSlackMessageAttachment) formatSlackMessage() slack.Attachment {
+	return slack.Attachment{
+		Pretext: fmt.Sprintf(`
+・プレイヤーID: %d
+・購入日時: %s
+・トランザクションID: %s
+・購入内容: %s
+`, sa.playerID, sa.formattedTime, sa.transactionID, sa.description,
+		),
+	}
 }
