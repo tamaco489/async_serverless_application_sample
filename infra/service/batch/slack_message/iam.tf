a5fb539612b22225d319ba99bcf7cb8006b09123
tamaco489 <tamaco489@gmail.com>
2025-03-14 23:43:59 +0900

    fix iam resource name

--- infra/service/batch/slack_message/iam.tf
+++ infra/service/batch/slack_message/iam.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role_policy_attachment" "slack_message_batch_execution_role" {
 # =================================================================
 # dynamodb policy
 # =================================================================
-data "aws_iam_policy_document" "slack_message_batch_policy" {
+data "aws_iam_policy_document" "slack_message_batch_dynamodb_policy" {
   statement {
     effect = "Allow"
     actions = [
@@ -47,15 +47,15 @@ data "aws_iam_policy_document" "slack_message_batch_policy" {
   }
 }
 
-resource "aws_iam_policy" "slack_message_batch_policy" {
+resource "aws_iam_policy" "slack_message_batch_dynamodb_policy" {
   name        = "${local.fqn}-batch-dynamodb-policy"
   description = "Allows Batch Lambda to read from the transaction_histories DynamoDB Stream"
-  policy      = data.aws_iam_policy_document.slack_message_batch_policy.json
+  policy      = data.aws_iam_policy_document.slack_message_batch_dynamodb_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "slack_message_batch_role" {
   role       = aws_iam_role.slack_message_batch.name
-  policy_arn = aws_iam_policy.slack_message_batch_policy.arn
+  policy_arn = aws_iam_policy.slack_message_batch_dynamodb_policy.arn
 }
 
 # =================================================================
