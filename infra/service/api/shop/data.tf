3f70fa1e14488a6093909e1294f233506b6ffef6
tamaco489 <tamaco489@gmail.com>
2025-04-05 18:56:18 +0900

    fix remote tfstate on shop-api

--- infra/service/api/shop/data.tf
+++ infra/service/api/shop/data.tf
@@ -29,3 +29,17 @@ data "terraform_remote_state" "sqs" {
     key    = "sqs/push_notification/terraform.tfstate"
   }
 }
+
+data "terraform_remote_state" "credential_line_message_api" {
+  backend = "s3"
+  config = {
+    bucket = "${var.env}-async-serverless-application-tfstate"
+    key    = "credential/line_message_api/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+# AWS マネージド型キー
+data "aws_kms_key" "secretsmanager" {
+  key_id = "alias/aws/secretsmanager"
+}
